2648e80e8f4a79e1df47983321934aa259adad98
Riya kumari <riya010206@gmail.com>
2025-05-19 02:24:10 +0530

    changes in vite file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:process.env.VITE_BASE_PATH || '/crypto Tracker App/'
+  base:process.env.VITE_BASE_PATH || '/Crypto-Tracker-App/'
 })
